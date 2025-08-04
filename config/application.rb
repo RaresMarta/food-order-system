b4624f051fbc34f33b6a4153f849156ac1e618cd
Rares Marta <rares.marta14@gmail.com>
2025-08-04 23:20:33 +0300

    Change test config from MiniTest to RSpec

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,11 @@ Bundler.require(*Rails.groups)
 module FoodOrderSystem
   class Application < Rails::Application
     config.load_defaults 8.0
-    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
